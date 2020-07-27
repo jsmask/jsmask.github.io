63ed7e610c61f2fdc1ff62764d50242a1b5892d5
jsmask <2674800533@qq.com>
2020-07-27 09:34:18 +0800

    update

--- svgatopngs/precache-manifest.28a4b129b88c4b23074e63497183e716.js
+++ svgatopngs/precache-manifest.28a4b129b88c4b23074e63497183e716.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dd9760582161f53a7c8b2ccc498b3cda",
+    "revision": "c5d6ea700d39bd9a13f25cb5d6c9c633",
     "url": "/svgatopngs/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/svgatopngs/static/css/2.fc5a9450.chunk.css"
   },
   {
-    "revision": "b1107457746dd6c88481",
+    "revision": "184ff80dd3fac87bffb6",
     "url": "/svgatopngs/static/css/main.30f304cb.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/svgatopngs/static/js/2.427579f2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b1107457746dd6c88481",
-    "url": "/svgatopngs/static/js/main.da1519fc.chunk.js"
+    "revision": "184ff80dd3fac87bffb6",
+    "url": "/svgatopngs/static/js/main.332fd4fa.chunk.js"
   },
   {
     "revision": "0aee515b3a69ad089f3a",
--- svgatopngs/service-worker.js
+++ svgatopngs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/svgatopngs/precache-manifest.0479c45bf366c61afcbc1ef213f7a06c.js"
+  "/svgatopngs/precache-manifest.28a4b129b88c4b23074e63497183e716.js"
 );
 
 self.addEventListener('message', (event) => {
